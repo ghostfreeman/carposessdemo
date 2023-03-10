1bd7a084bdc4cc6ec30828443dd71baa4bfe508a
Cameron Kilgore <ghostfreeman@gmail.com>
2023-03-10 17:22:16 -0600

    added vehicle controller and flatgrass test scene

--- spedometer.gd
+++ spedometer.gd
@@ -23,13 +23,13 @@ func _ready():
 func _process(_delta):
 	var speed := car_body.linear_velocity.length()
 	if speed_unit == SpeedUnit.METERS_PER_SECOND:
-		text = "Speed: " + ("%.1f" % speed) + " m/s"
+		text = ("%.1f" % speed) + " m/s"
 	elif speed_unit == SpeedUnit.KILOMETERS_PER_HOUR:
 		speed *= 3.6
-		text = "Speed: " + ("%.0f" % speed) + " km/h"
+		text = ("%.0f" % speed) + " KM/H"
 	else: # speed_unit == SpeedUnit.MILES_PER_HOUR:
 		speed *= 2.23694
-		text = "Speed: " + ("%.0f" % speed) + " mph"
+		text = ("%.0f" % speed) + " MPH"
 
 	# Change speedometer color depending on speed in m/s (regardless of unit).
 	add_theme_color_override("font_color", gradient.sample(remap(car_body.linear_velocity.length(), 0.0, 30.0, 0.0, 1.0)))
